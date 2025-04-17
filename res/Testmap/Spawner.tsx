7420768c68c7894756496795e38c025af32b3875
freathedge <freathedge@gmail.com>
2025-04-18 00:58:23 +0200

    finish feature fasterOnPath

---
+++ res/Testmap/Spawner.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Spawner" tilewidth="50" tileheight="50" tilecount="1" columns="1">
+ <image source="images/spawner.png" width="50" height="50"/>
+</tileset>
